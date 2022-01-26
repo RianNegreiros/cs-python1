1eb294a09edb5c5ff680385ce09bcac5b6720236
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-01-26 19:44:40 -0300

    improve code

--- cs-week4/opEx1.py
+++ cs-week4/opEx1.py
@@ -1,11 +1,15 @@
+from math import sqrt
+
 n = 0
 while n < 2:
     n = int(input("Digite um número: "))
 
-for i in range(2, n):
-    if n % i == 0:
-        print("não primo")
+prime = 0
+for i in range(2, int(sqrt(n)) + 1):
+    if (n % i) == 0:
+        prime = 1
         break
-    else:
-        print("primo")
-        break
+if prime == 0:
+    print("primo")
+else:
+    print("não primo")
