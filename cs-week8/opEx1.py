f046dda3fd8be6bc36b19ebe2f36e986be60c57b
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-01-28 23:47:53 -0300

    maximum_value

--- cs-week8/opEx1.py
+++ cs-week8/opEx1.py
@@ -1,2 +1,10 @@
 def maior_elemento(arr):
     return max(arr)
+
+
+def maximum_value(arr):
+    max_value = 0
+    for i in range(0, len(arr)):
+        if arr[i] > max_value:
+            max_value = arr[i]
+    return max_value
