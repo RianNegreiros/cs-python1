4b10e52378f96d33977fe514b1860ce7e2bf8e0f
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-01-29 21:07:04 -0300

    hypotenuse sum

---
+++ cs-week7/opEx2.py
@@ -0,0 +1,29 @@
+from math import sqrt
+
+
+def e_hipotenusa(n):
+    while n % 2 == 0:
+        n = n // 2
+
+    for i in range(3, int(sqrt(n)) + 1, 2):
+        if n % i == 0:
+            if (i - 1) % 4 == 0:
+                return True
+
+            while n % i == 0:
+                n = n // i
+
+    if n > 2 and (n - 1) % 4 == 0:
+        return True
+
+    else:
+        return False
+
+
+def soma_hipotenusas(n):
+    arr = []
+    for i in range(1, n + 1):
+        if e_hipotenusa(i):
+            print(i)
+            arr.append(i)
+    return sum(arr)
